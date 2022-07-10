5234a16f1c4951847ef81d15fa0adf4b1fc3129b
Дастанбекулы Фараби Дастанбекович <80412019+hauntff@users.noreply.github.com>
2022-07-11 00:15:42 +0600

    first commit

---
+++ TestTaskForProjectJob/ImportDataModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTaskForProjectJob
+{
+    public class ImportDataModel
+    {
+        public string Category { get; set; }
+        public string Desc { get; set; }
+        public int Code { get; set; }
+        public string Brand { get; set; }
+        public string Size { get; set; }
+        public int UPC { get; set; }
+        public DateTime Last_Sold_On { get; set; }
+        public decimal Price { get; set; }
+        public decimal Sale { get; set; }
+        public DateTime Begins { get; set; }
+        public DateTime Expires { get; set; }
+        public int Max { get; set; }
+        public decimal CasePrice { get; set; }
+        public decimal CaseSale { get; set; }
+        public DateTime CBegins { get; set; }
+        public DateTime CExpires { get; set; }
+        public int CMax { get; set; }
+        public int Upp { get; set; }
+        public decimal PkgPrc { get; set; }
+        public int SUpp { get; set; }
+        public string Department { get; set; }
+        public decimal SPkgPrc { get; set; }
+        public DateTime PBegins { get; set; }
+        public DateTime PExpires { get; set; }
+        public int PMax { get; set; }
+        public string Measure { get; set; }
+        public decimal Units { get; set; }
+        public bool FS { get; set; }
+        public bool Tax { get; set; }
+        public bool Wic { get; set; }
+        public string OnHand { get; set; }
+        public string Season { get; set; }
+        public string PrdRule { get; set; }
+        public string Food_For_Who { get; set; }
+        
+        public static ImportDataModel FromCsv(string row)
+        {
+            string[] values = row.Split('|');
+        }
+    }
+}
---
+++ TestTaskForProjectJob/Program.cs
@@ -0,0 +1,41 @@
+﻿using ClosedXML.Excel;
+using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+
+namespace TestTaskForProjectJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\import_data.csv";
+            string[] randomWords = { "EARTH", "MARS", "JUPITER", "VENUS", "SATURN" };
+            Random random = new Random();
+            List<string> rows = File.ReadAllLines(path)
+                .ToList();
+
+            for (int i = 0; i < rows.Count; i++)
+            {
+                if (i == 0)
+                {
+                    rows[i] = rows[i] + " | " + "FoodForWho";
+                    continue;
+                }
+
+                rows[i] = rows[i] + " | " + randomWords[random.Next(0, randomWords.Length - 1)];
+            }
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+                sw.Write(string.Join("\r\n", rows));
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
